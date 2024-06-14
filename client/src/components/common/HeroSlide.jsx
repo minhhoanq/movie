928734da6edd0fe761458dbe572f80a23870843a
minhhoanq <mh.tranminhhoang@gmail.com>
2024-06-14 21:27:16 +0700

    fe hero slide

--- client/src/components/common/HeroSlide.jsx
+++ client/src/components/common/HeroSlide.jsx
@@ -15,7 +15,7 @@ import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { toast } from "react-toastify";
 import { setGlobalLoading } from "../../redux/features/globalLoadingSlice";
-import { routeGen } from "../../routes/routes";
+import { routesGen } from "../../routes/routes";
 import uiConfigs from "../../configs/ui.config";
 import CircularRate from "./CircularRate";
 import tmdbConfigs from "../../api/configs/tmdb.config";
@@ -76,15 +76,15 @@ const HeroSlide = ({ mediaType, mediaCategory }) => {
             <Swiper
                 grabCursor={true}
                 loop={true}
-                modules={[Autoplay]}
+                // modules={[Autoplay]}
                 style={{
                     width: "100%",
                     height: "max-content",
                 }}
-                autoplay={{
-                    delay: 3000,
-                    disableOnInteraction: false,
-                }}
+                // autoplay={{
+                //     delay: 3000,
+                //     disableOnInteraction: false,
+                // }}
             >
                 {movies.map((movie, index) => (
                     <SwiperSlide key={index}>
@@ -102,7 +102,131 @@ const HeroSlide = ({ mediaType, mediaCategory }) => {
                                     movie.backdrop_path || movie.poster_path
                                 )})`,
                             }}
-                        ></Box>
+                        />
+                        <Box
+                            sx={{
+                                width: "100%",
+                                height: "100%",
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                ...uiConfigs.style.horizontalGradientBgImage[
+                                    theme.palette.mode
+                                ],
+                            }}
+                        />
+                        <Box
+                            sx={{
+                                width: "100%",
+                                height: "100%",
+                                position: "absolute",
+                                top: 0,
+                                left: 0,
+                                paddingX: {
+                                    sm: "10px",
+                                    md: "5rem",
+                                    lg: "10rem",
+                                },
+                            }}
+                        >
+                            <Box
+                                sx={{
+                                    height: "100%",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    paddingX: "30px",
+                                    color: "text.primary",
+                                    width: {
+                                        sm: "unset",
+                                        md: "30%",
+                                        lg: "40%",
+                                    },
+                                }}
+                            >
+                                <Stack spacing={4} direction={"column"}>
+                                    {/* title */}
+                                    <Typography
+                                        variant="h4"
+                                        fontSize={{
+                                            xs: "2rem",
+                                            md: "2rem",
+                                            lg: "4rem",
+                                        }}
+                                        fontWeight={700}
+                                        sx={{
+                                            ...uiConfigs.style.typoLines(
+                                                2,
+                                                "left"
+                                            ),
+                                        }}
+                                    >
+                                        {movie.title || movie.name}
+                                    </Typography>
+                                    {/* title */}
+                                    <Stack
+                                        direction={"row"}
+                                        spacing={1}
+                                        alignItems={"center"}
+                                    >
+                                        {/* rating */}
+                                        <CircularRate
+                                            value={movie.vote_average}
+                                        />
+                                        {/* rating */}
+                                        <Divider orientation="vertical" />
+                                        {/* genres */}
+                                        {[...movie.genre_ids]
+                                            .splice(0, 2)
+                                            .map((genreId, index) => (
+                                                <Chip
+                                                    variant="filled"
+                                                    color="primary"
+                                                    key={index}
+                                                    label={
+                                                        genres.find(
+                                                            (e) =>
+                                                                e.id === genreId
+                                                        ) &&
+                                                        genres.find(
+                                                            (e) =>
+                                                                e.id === genreId
+                                                        ).name
+                                                    }
+                                                />
+                                            ))}
+                                        {/* genres */}
+                                    </Stack>
+                                    {/* overview */}
+                                    <Typography
+                                        variant="body1"
+                                        sx={{
+                                            ...uiConfigs.style.typoLines(3),
+                                        }}
+                                    >
+                                        {movie.overview}
+                                    </Typography>
+                                    {/* overview */}
+
+                                    {/* watch now */}
+                                    <Button
+                                        variant="contained"
+                                        size="large"
+                                        startIcon={<PlayArrowIcon />}
+                                        component={Link}
+                                        to={routesGen.mediaDetail(
+                                            mediaType,
+                                            movie.id
+                                        )}
+                                        sx={{
+                                            width: "max-content",
+                                        }}
+                                    >
+                                        Watch now
+                                    </Button>
+                                    {/* watch now */}
+                                </Stack>
+                            </Box>
+                        </Box>
                     </SwiperSlide>
                 ))}
             </Swiper>
